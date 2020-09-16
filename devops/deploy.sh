01bc420671ebb078583712c1d931d23b7b37931e
Edward Butler <Edward.Butler@endava.com>
2020-09-16 15:08:26 +0100

    simpler times

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,13 +2,19 @@
 
 set -e
 
-npm install -g npm
-npm install
+COMMIT8=${TRAVIS_COMMIT::8}
+COMMIT=${TRAVIS_COMMIT}
+
+#npm install -g npm
+#npm install
 #npm list
 #npm run deploy
 
 docker build -t ed201971/godaddy devops/.
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-npx semantic-release
+#npx semantic-release
+
+echo ¢COMMIT8
+echo ¢COMMIT
 
 #docker push ed201971/godaddy
